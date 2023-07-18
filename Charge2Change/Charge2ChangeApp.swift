d8aad905d04ca9aacee52dcb8c14624e4af41571
ahmetsenturk <iahmetsenturk@yahoo.com>
2023-07-18 18:30:35 +0200

    Initial Commit

---
+++ Charge2Change/Charge2ChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charge2ChangeApp.swift
+//  Charge2Change
+//
+//  Created by Ahmet Senturk on 18.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Charge2ChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
